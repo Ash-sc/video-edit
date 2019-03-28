e3933af24b3b67c6659eef1680b1fe0459750b18
Ash-sc <mail@ashshen.cc>
2019-03-28 11:26:05 +0800

    rebuild

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
-import Element from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
 import './assets/main.less'
+import { Input, Button, Message } from 'element-ui'
 
-Vue.use(Element)
+Vue.use(Input)
+Vue.use(Button)
+
+Vue.prototype.$message = Message
 
 Vue.config.productionTip = false
 
